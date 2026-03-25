01ecf66cb15a81aba833fc3825ca2c8ce1f6ffd7
danish614 <danish.ilyas9167890@gmail.com>
2026-03-25 21:33:12 +0500

    Serve media files via WhiteNoise in production

--- restaurant_project/restaurant/wsgi.py
+++ restaurant_project/restaurant/wsgi.py
@@ -3,9 +3,14 @@ WSGI config for restaurant project.
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant.settings')
 
 application = get_wsgi_application()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=str(BASE_DIR / 'media'), prefix='media/')
